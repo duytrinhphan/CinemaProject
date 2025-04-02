be2177815284725f8769a235756c4ef92bb874cd
nguyenxuanthinh4803 <nguyenxuanthinh.4803@gmail.com>
2025-04-02 19:29:25 +0700

    Created Hubs folder, Datve.cs

---
+++ DuAnRapChieuPhim/Hubs/DatVe.cs
@@ -0,0 +1,45 @@
+using DuAnRapChieuPhim.Models;
+using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace DuAnRapChieuPhim.Hubs
+{
+    public class DatVe : Hub
+    {
+        private static ConcurrentDictionary<string, Timer> seatTimers = new ConcurrentDictionary<string, Timer>();
+
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void UpdateSeatStatus(string MaGhe, string status)
+        {
+            Clients.All.receiveSeatStatus(MaGhe, status);
+        }
+
+        public void SelectSeat(string MaGhe)
+        {
+            Clients.Others.lockSeat(MaGhe);
+
+            Timer timer = new Timer(TimerCallback, MaGhe, 30000, Timeout.Infinite);
+            seatTimers[MaGhe] = timer;
+        }
+
+        public void DeselectSeat(string MaGhe)
+        {
+            Clients.Others.unlockSeat(MaGhe);
+
+            if (seatTimers.TryRemove(MaGhe, out Timer timer))
+            {
+                timer.Dispose();
+            }
+        }
+    }
+}
